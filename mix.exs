29ecb7efadefd986364d288b35dcbceb4b762de5
Sean Tan <sean@yojee.com>
2025-10-20 13:13:11 +0800

    fix: based on https://github.com/elixirs/faker/pull/623/changes/7a80bbffbe221145c179fbcf09143db91a164892
    - https://github.com/elixirs/faker/pull/623

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Faker.Mixfile do
   use Mix.Project
 
   @source_url "https://github.com/elixirs/faker"
-  @version "0.19.0-alpha.1"
+  @version "0.18.0"
 
   def project do
     [
@@ -14,11 +14,6 @@ defmodule Faker.Mixfile do
       name: "Faker",
       deps: deps(),
       docs: docs(),
-      source_url: @source_url,
-      homepage_url: @source_url,
-      preferred_cli_env: [
-        "test.watch": :test
-      ],
       dialyzer: [
         flags: [
           :error_handling,
@@ -29,10 +24,17 @@ defmodule Faker.Mixfile do
     ]
   end
 
+  def cli do
+    [
+      preferred_envs: [
+        "test.watch": :test
+      ]
+    ]
+  end
+
   def application do
     [
-      # mod: {Faker.Application, []},
-      extra_applications: [:crypto, :makeup],
+      applications: [:crypto],
       env: env()
     ]
   end
@@ -47,13 +49,11 @@ defmodule Faker.Mixfile do
 
   defp deps do
     [
-      {:credo, "== 1.7.11", only: [:dev, :test], runtime: false},
-      {:dialyxir, "== 1.4.5", only: [:dev], runtime: false},
+      {:ex_doc, "== 0.36.1", only: :dev, runtime: false},
       {:earmark, "== 1.4.47", only: :dev, runtime: false},
-      {:ex_doc, "== 0.37.0", only: :dev, runtime: false},
-      {:makeup, "== 1.2.1"},
-      {:makeup_elixir, "== 1.0.1"},
-      {:mix_test_watch, "== 1.2.0", only: [:dev, :test], runtime: false}
+      {:credo, "== 1.7.15", only: [:dev, :test], runtime: false},
+      {:dialyxir, "== 1.4.5", only: [:dev], runtime: false},
+      {:mix_test_watch, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 
@@ -68,11 +68,11 @@ defmodule Faker.Mixfile do
   end
 
   defp package do
-    [
+    %{
       maintainers: ["Anthony Smith", "Igor Kapkov", "Toby Hinloopen", "Vitor Oliveira"],
       files: ["lib", "mix.exs", "mix.lock", "README.md", "LICENSE", "CHANGELOG.md"],
       licenses: ["MIT"],
       links: %{"GitHub" => @source_url}
-    ]
+    }
   end
 end
